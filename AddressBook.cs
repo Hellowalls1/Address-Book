72f4a1c2458cab67f3c0cf1f988823bf84c83663
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-31 19:57:37 -0500

    initial commit

---
+++ AddressBook.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+//everything is in the namespace so Building can be called in City.cs
+namespace Address_Book {
+
+    public class AddressBook {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+
+    public class Dictionary<string, string> adresses = Dictionary<string, string> () {
+
+    }
+
+}
---
+++ Contact.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+//everything is in the namespace so Building can be called in City.cs
+namespace Address_Book {
+
+    public class Contact {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+
+}
