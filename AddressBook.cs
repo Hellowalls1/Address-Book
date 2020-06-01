86fcd17dc97d3b986584c305e302e89134aa0232
Hellowalls1 <thesingingbutcher@gmail.com>
2020-06-01 08:12:25 -0500

    flailed

--- AddressBook.cs
+++ AddressBook.cs
@@ -4,16 +4,7 @@ using System.Collections.Generic;
 //everything is in the namespace so Building can be called in City.cs
 namespace Address_Book {
 
-    public class AddressBook {
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-
-    }
-
-    public class Dictionary<string, string> adresses = Dictionary<string, string> () {
+    public class Dictionary<string, Contact> AllContacts = Dictionary<string, Contact> () {
 
     }
 
